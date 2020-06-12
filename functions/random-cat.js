a8c6ed8eee275595d3b6814e4e6de488073e1637
Emily Till <emileefrances@hotmail.co.uk>
2020-06-12 16:07:55 +0100

    changed typo

--- functions/random-cat.js
+++ functions/random-cat.js
@@ -1,9 +1,9 @@
 const catFacts = [
   "cats spend 70% of their lives asleep",
-  " In 1963 a called called Felicette went to space",
-  " The oldest cat, Creme Puff, reached 38 years of age",
-  " Cats can rotate their ears 180 degrees",
-  " Most cats have no eyelashes",
+  "In 1963 a called called Felicette went to space",
+  "The oldest cat, Creme Puff, reached 38 years of age",
+  "Cats can rotate their ears 180 degrees",
+  "Most cats have no eyelashes",
 ];
 
 exports.handler = async (events) => {
